ba95e838bad706d7a817a53b1f42d264665ccdec
Krymorn <cymartinbell@gmail.com>
2026-07-01 11:08:33 -0400

    Updated to 1.8.0
    Updated file paths and changed map file format to JSON. Improved character filtering and added handling for additional delimiters.

--- mapping.py
+++ mapping.py
@@ -1,33 +1,39 @@
 # Input and map file paths
 # Change to "v101_cleaned.txt" and "v101_mapping.txt" if you would like.
-v101FilePath = "eva_cleaned.txt"
-mapFilePath = "eva_mapping.txt"
+filePath = "eva_cleaned.txt"
+mapFilePath = "eva_mapping.json"
 
 # Setup foundChars array
 foundChars = []
 
 # Find characters in the input file and save to foundChars
-with open(v101FilePath, "r") as v101File:
+with open(filePath, "r", encoding="utf-8") as file:
 
-  for char in v101File.read():
+  for char in file.read():
 
     if not char in foundChars:
       foundChars.append(char)
+
+foundChars.sort()
 
 # Wipe map file
 mapFile = open(mapFilePath, "w")
 mapFile.close()
 
 # For each character in foundChars, write the character into the map file
-with open(mapFilePath, "a") as mapFile:
+with open(mapFilePath, "w", encoding="utf") as mapFile:
 
-  i = 0
+  mapFile.write("{\n")
+  
   for char in foundChars:
 
-    # Skips input file delimiters
-    if char == "\n" or char == "." or char == ",":
-      i = i - 1
+    # Skips input file delimiters and end of line delimters and other misilanious characters
+    if char == "\n" or char == "." or char == "," or char == "\"" or char == "-" or char == "=":
+      pass
     else:
-      mapFile.write(str(i) + "=" + char + "~" + char + "\n")
-    
-    i = i + 1
+      if char == "\\":
+        mapFile.write("    \"\\" + char + "\": \"\\" + char + "\",\n")
+      else:
+        mapFile.write("    \"" + char + "\": \"" + char + "\",\n")
+
+  mapFile.write("}")
